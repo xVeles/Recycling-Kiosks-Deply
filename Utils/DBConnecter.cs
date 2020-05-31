fb5f52cf67bf770f3e31935973bf6b2ed9b9c6a0
xVeles <shankuah62@gmail.com>
2020-05-31 13:12:28 +1200

    Retry dependacies

---
+++ Utils/DBConnecter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+
+namespace Recycling_Kiosk.Utils
+{
+    public class DBConnecter
+    {
+        public static SQLiteConnection DBConnect()
+        {
+            ConfigReader configReader = new ConfigReader();
+            SQLiteConnection sqliteConnection = new SQLiteConnection(string.Format("Data Source={0};Version=3;", configReader.GetString("DataRoot") + "data.db"));
+            sqliteConnection.Open();
+            return sqliteConnection;
+        }
+    }
+}
